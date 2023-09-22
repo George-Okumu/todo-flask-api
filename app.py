11c7b5aaf700763b07290f5fdbd8a29fbf8f2321
George-Okumu <gokumu12@gmail.com>
2023-09-22 05:47:07 +0300

    created methods & routes for creating a new user, updating a user, deleting a user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -31,6 +31,7 @@ def users():
     users_ar = []
     for user in User.query.all():
         user_object = {
+            'id': user.id,
             'name':user.name,
             'email':user.email
         }
@@ -45,6 +46,87 @@ def users():
     #    }
     #    for user in User.query.all()
     # ])
+
+@app.route('/add-user', methods=["POST"])
+def create_user():
+    user = request.get_json()
+
+    new_user = User(
+        name = user['name'],
+        email = user['email']
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.id,
+        "name": new_user.name,
+        "email": new_user.email
+    }), 201
+
+
+@app.route('/update-user/<int:id>', methods=["PUT"])
+def update_user(id):
+    new_user_details = request.get_json()
+    user_from_db = User.query.filter_by(id=id).first_or_404()
+
+    user_from_db.name = new_user_details['name']
+    user_from_db.email = new_user_details['email']
+
+    db.session.commit()
+
+    return jsonify({
+        "id": user_from_db.id,
+        "name": user_from_db.name,
+        "email": user_from_db.email
+    }), 200
+
+
+@app.route('/delete-user/<int:id>', methods=["DELETE"])
+def delete_user(id):
+    user_to_delete = User.query.filter_by(id=id).first()
+    
+
+    # return jsonify({"message": "user deleted successfully"}), 200
+
+    if(user_to_delete):
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        return jsonify({"message": "user deleted successfully"}), 200
+    else:
+        return make_response(
+            jsonify({
+                "error": "user not found"
+            }), 404
+        )
+    
+
+    
+    
+
+# @app.route('/add-user', methods=["POST"])
+# def create_user():
+#     new_user = User(
+#         name = request.form.get("name"),
+#         email = request.form.get("email")
+#     )
+
+#     db.session.add(new_user)
+#     db.session.commit()
+
+#     resp = {
+#         "message": "user created succesfully",
+#         "user": {
+#         'id': new_user.id,
+#         'name': new_user.name,
+#         'email': new_user.email
+#         }
+#     }
+
+#     return make_response(
+#         jsonify(resp), 201
+#     )
 
 
 if __name__ == '__main__':
